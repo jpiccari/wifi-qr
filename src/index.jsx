5f4b6e131ed0de4378dbe7f15fcdf88d46e1ff5d
Joshua Piccari <joshua.piccari@gmail.com>
2023-04-24 18:07:14 -0700

    Clean up site and add debounce

--- src/index.jsx
+++ src/index.jsx
@@ -1,17 +1,28 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {createRoot} from 'react-dom/client';
 import QRCode from 'react-qr-code';
+import debounce from 'lodash.debounce';
 
-const qrCode = ReactDOM.createRoot(document.getElementById("qr-code"));
+const MAX_LENGTH = 8192;
+const qrCode = createRoot(document.getElementById("qr-code"));
 const qrInput = document.getElementById("qr-code-value");
 
-function tick() {
+const tick = debounce(() => {
+    console.log('hi')
+    const value = qrInput.value;
+    if (value.length > MAX_LENGTH) {
+        alert("Value to long...");
+        qrInput.value = qrInput.value.substr(0, MAX_LENGTH);
+        return;
+    }
+
     const element = <QRCode
     value={qrInput.value}
-    size="800"
+    size="100%"
     level="H"/>;
     qrCode.render(element);
-}
+}, 200, {maxWait: 2000});
 
-qrInput.oninput = () => tick();
+qrInput.oninput = tick;
 
+tick();
